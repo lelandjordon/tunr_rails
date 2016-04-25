dc22a8f58879e3ffe897d9ccde51e711ebd7c7e0
Leland Jordon <lelandjordon@mac.com>
2016-04-25 13:14:02 -0400

    did a lot of following in class.

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,5 @@
 class Playlist < ActiveRecord::Base
   has_many :playlist_entries
   # ADD CODE TO ASSOCIATE PLAYLISTS TO ENTRIES AND SONGS
+  has_many :songs, through: :playlist_entries
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   # ADD CODE TO ASSOCIATE PLAYLISTS TO ENTRIES AND SONGS
+  has_many :playlist_entries
+  has_many :playlists, through: :playlist_entries
 end
